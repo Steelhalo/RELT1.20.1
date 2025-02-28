58a5e72eeafc3a33da76b0558dc3b025ffda28c3
Steelhalo <Dukeevan806@gmail.com>
2025-02-28 20:20:08 +0000

    Change src directory

--- src/main/kotlin/griglog/relt/RELT.kt
+++
@@ -1,8 +0,0 @@
-package griglog.relt
-import net.minecraft.resources.ResourceLocation
-import org.apache.logging.log4j.LogManager
-
-object RELT{
-    const val id = "roughly_enough_loot_tables"
-    val logger = LogManager.getLogger(id)
-}
--- src/main/kotlin/griglog/relt/Utils.kt
+++
@@ -1,9 +0,0 @@
-package griglog.relt
-
-import net.minecraft.core.component.DataComponents
-import net.minecraft.network.chat.Component
-import net.minecraft.world.item.ItemStack
-
-fun wrapHoverName(stack: ItemStack){
-    stack.set(DataComponents.CUSTOM_NAME, Component.literal("*").append(stack.hoverName).append("*"))
-}
