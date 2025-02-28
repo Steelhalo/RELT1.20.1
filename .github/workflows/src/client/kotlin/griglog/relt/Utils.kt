7c1c7d6f1b29858c5e0509498ebe9c5a1ddd1aaa
Steelhalo <Dukeevan806@gmail.com>
2025-02-28 20:19:06 +0000

    Delete .github/workflows directory

--- .github/workflows/src/client/kotlin/griglog/relt/Utils.kt
+++
@@ -1,8 +0,0 @@
-package griglog.relt
-
-import net.minecraft.network.chat.Component
-import net.minecraft.world.item.ItemStack
-
-fun wrapHoverName(stack: ItemStack){
-    stack.hoverName = Component.literal("*").append(stack.hoverName).append("*")
-}
